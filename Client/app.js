f9816d7c208ba719fbba142002ae0d18cd63edbe
unknown <jeff.a.cate@gmail.com>
2020-04-01 15:11:40 -0500

    Polished UX/UI view for: Add, Details, and Delete functions.

--- Client/app.js
+++ Client/app.js
@@ -8,7 +8,7 @@
         };
 
         $.ajax({
-            url: 'https://localhost:44325/api/movie/',
+            url: 'https://localhost:44325/api/movie',
             dataType: 'json',
             type: 'post',
             contentType: 'application/json',
@@ -19,15 +19,25 @@
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
-        });
+        }).then(location.reload());
 
         e.preventDefault();
     }
+    
     $('#my-form').submit( processForm );
   })(jQuery);
 
+  
    $("#getMovieButton").click(function(){
+    var edit = document.getElementById("editContainer");
+    var add = document.getElementById("addContainer");
 
+    if (edit.style.display === "block") {
+      edit.style.display = "none";
+    }
+    if (add.style.display === "block"){
+      add.style.display === "none";
+    }
       let id = $("#movieSelector option:selected").val();
 
     $.ajax({
@@ -36,19 +46,82 @@
       type: 'GET'
     }).then(function(data){
       
+      $("#search").html("");
       let title = data["title"];
       let genre = data["genre"];
       let director = data["director"];
-      var header = $(" <tr class='text-center bg-info text-dark col-sm-12'><th> " + title + " </th> <th> " + genre + "</th><th> " + director + "</th><tr> ");
-      // alert("title: " + title + "\n" +
-      //       "genre: " + genre + "\n" +
-      //       "director: " + director + "\n");
-      $("#search").append(header);
+      if(title !== undefined && genre !== undefined && director !== undefined){
+        var header = $(` <tr class=' bg-info text-center text-dark'><th> ` + title + ` </th> <th> ` + genre + `</th><th> ` + director + `</th>`);
+        $("#search").append(header);
+      }
+      
 
     });
     
   });
 
+  $('#editMovieButton').click(function(e){
+
+    
+    let id = $("#movieSelector option:selected").val();
+    
+    $("#search").html("");
+    var edit = document.getElementById("editContainer");
+    var add = document.getElementById("addContainer");
+  if (edit.style.display === "none" && id !== "") {
+    edit.style.display = "block";
+  } else {
+    edit.style.display = "none";
+  }
+  if (add.style.display === "block") {
+    add.style.display = "none";
+  }
+    // display movie
+
+    $.ajax({
+      url: 'https://localhost:44325/api/movie/'+ id,
+      dataType: 'json',
+      type: 'GET'
+    }).then(function(data){
+      
+      $("#search").html("");
+      let title = data["title"];
+      let genre = data["genre"];
+      let director = data["director"];
+      if(title !== undefined && genre !== undefined && director !== undefined){
+      var header = $(` <tr class=' bg-warning text-center text-dark'><th> ` + title + ` </th> <th> ` + genre + `</th><th> ` + director + `</th>`);
+      $("#search").append(header);
+      }
+    });
+  });
+
+  $('#addMovieButton').click(function(e){
+    
+    $("#search").html("");
+  
+    var add = document.getElementById("addContainer");
+    var edit = document.getElementById("editContainer");
+  if (add.style.display === "none") {
+    add.style.display = "block";
+  } else {
+    add.style.display = "none";
+  }
+  if (edit.style.display === "block") {
+    edit.style.display = "none";
+  }
+  });
+
+  $('#deleteMovieButton').click(function(e){
+    
+    let id = $("#movieSelector option:selected").val();
+    $.ajax({
+      url: 'https://localhost:44325/api/movie/' + id,
+      type: 'delete',
+      success: function(response){
+        location.reload();
+      }
+    });
+  });
   
 $(document).ready(function(){
   $.ajax({
@@ -66,7 +139,9 @@ $(document).ready(function(){
           let title = data[i]["title"];
           let genre = data[i]["genre"];
           let director = data[i]["director"];
+          
           var movie = $("<tr class='text-center'><td>"+ title +"</td><td>"+ genre +" </td><td> "+ director +"</td>");
+          $('#movieSelector').append(`<option value='${data[i].movieId}'>${title}</option>`);
           $("#response #movies").append(movie);
         }
 
